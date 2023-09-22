e835179a656b89125b0ecd78a325b637f9040b53
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-09-22 17:39:58 -0300

    queue_len implemented

--- srcs/server/decode/bin_to_char.c
+++ srcs/server/decode/bin_to_char.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 16:09:03 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/09/22 17:13:35 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/09/22 17:39:20 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ char	bin_to_char(t_queue **front)
 
 	if (!*front)
 		return (-1);
+	if (queue_len(front) < 8)
+	{	
+		fp_printf("Error: Queue doesn't have enough bits for a char character\n");
+		return (-1);
+	}
 	n = 7;
 	output = '\0';
 	bin = dequeue_fauna(front);
--- srcs/server/tests/bin_to_char_test.c
+++ srcs/server/tests/bin_to_char_test.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 16:00:11 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/09/22 17:13:43 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/09/22 17:36:11 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,27 @@ MU_TEST(test_bin_to_char_conversion)
 	mu_check(output == 'A');
 }
 
+MU_TEST(test_incorrect_queue_len)
+{
+	t_queue *front;
+	char	output;
+
+	front = NULL;
+	enqueue_pre_alloc(&front, 0);
+	enqueue_pre_alloc(&front, 1);
+	enqueue_pre_alloc(&front, 0);
+	enqueue_pre_alloc(&front, 0);
+	enqueue_pre_alloc(&front, 0);
+	enqueue_pre_alloc(&front, 0);
+	enqueue_pre_alloc(&front, 0);
+	output = bin_to_char(&front);
+	mu_check(output == -1);
+}
+
 MU_TEST_SUITE(conversion_test)
 {
 	MU_RUN_TEST(test_bin_to_char_conversion);
+	MU_RUN_TEST(test_incorrect_queue_len);
 }
 
 int main(void)
